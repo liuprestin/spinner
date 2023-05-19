a84d8153369e7f3d2c21eb6d0e7775c6caaaeaf1
Prestin Liu <liuprestin@proton.me>
2023-05-18 22:27:14 -0700

    adding the spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,21 @@
+
+for(let x = 3; x > 0; x--) {
+    setTimeout(() => {
+      process.stdout.write('\r|   ');
+    }, 100);
+    
+    setTimeout(() => {
+      process.stdout.write('\r/   ');
+    }, 300);
+    
+    setTimeout(() => {
+      process.stdout.write('\r-   ');
+    }, 500);
+    
+    setTimeout(() => {
+      // Need to escape the backslash since it's a special character.
+      process.stdout.write('\r\\   '); 
+    }, 700);
+  }
+
+  
